08232e593c923674f741dfee2327a3912d09e1c0
Matthew Butcher <99278651+mbutcherdev@users.noreply.github.com>
2023-03-08 12:24:54 +0000

    Initial framework for django

--- budgetapp/budgetapp_project/pages/views.py
+++ budgetapp/budgetapp_project/pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("<h1>Budget App Homepage Routing Active</h1>")
